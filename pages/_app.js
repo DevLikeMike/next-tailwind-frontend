582a273d65c45b9878e7277fe14a8c9c25383a4a
DevLikeMike <mhall.project@gmail.com>
2022-04-30 12:54:37 -0400

    Initial set up for tailwind and sass, added header and layout

---
+++ pages/_app.js
@@ -0,0 +1,13 @@
+import "../styles/index.scss";
+import "../styles/tailwind.css";
+import Layout from "../components/Layout";
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
+}
+
+export default MyApp;
---
+++ pages/index.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className='bg-gray-300'>
+      <h1>testy test</h1>
+    </div>
+  );
+}
